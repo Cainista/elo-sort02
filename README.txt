9565d2cff93c1182980d2474ecd7bda238e057e9
Chubasco <Chubasco@gmail.com>
2020-08-10 06:52:15 -0400

    Funciona ok

--- README.txt
+++ README.txt
@@ -7,3 +7,4 @@ https://riptutorial.com/c/example/8274/get-lines-from-a-file-using-getline--
 https://www.quora.com/How-do-I-break-or-terminate-out-of-a-for-loop-in-C-during-runtime
 https://www.thegeekstuff.com/2013/01/c-argc-argv/
 https://www.geeksforgeeks.org/recursive-insertion-sort/
+https://www.geeksforgeeks.org/heap-sort/
--- sortingComparer.c
+++ sortingComparer.c
@@ -3,6 +3,11 @@
 
 #define MAX_NUMBERS 10000
 
+// Funciones
+int imprimeArreglo();
+void insertionSort();
+void heapSort();
+
 // Bloque principal
 int main(int argc, char* argv[]){
   // Verificamos sintaxis
@@ -34,12 +39,105 @@ int main(int argc, char* argv[]){
     //Muestra los datos del .txt
     while (getline(&line_buf, &line_buf_size, fptr) > 0 ) {
       numbers_array[cc++]=atoi(line_buf);
-      printf("%03d)\t%06d\n", cc, numbers_array[cc-1]);
+      // printf("%03d)\t%06d\n", cc, numbers_array[cc-1]);
       if (cc>=MAX_NUMBERS)
         break;
     }
+    fclose(fptr);
 
+    imprimeArreglo(numbers_array, cc);
+    insertionSort(numbers_array, cc);
+    imprimeArreglo(numbers_array, cc);
+    heapSort(numbers_array, cc);
+    imprimeArreglo(numbers_array, cc);
 
-   fclose(fptr);
+    // Imprime numeros desordenados
+
   return 0;
+} // main
+
+
+int imprimeArreglo(int *numArray, int size){
+  for (int i = 0; i < size; i++)
+    printf("%03d: %06d\n",i+1, numArray[i]);
+  return 0;
+}
+
+
+void insertionSort(int *numArray, int size){
+    // Base case
+    if (size <= 1)
+        return;
+
+    // Sort first n-1 elements
+    insertionSort( numArray, size-1 );
+
+    // Insert last element at its correct position
+    // in sorted array.
+    int last = numArray[size-1];
+    int j = size-2;
+
+    /* Move elements of arr[0..i-1], that are
+      greater than key, to one position ahead
+      of their current position */
+    while (j >= 0 && numArray[j] > last)
+    {
+        numArray[j+1] = numArray[j];
+        j--;
+    }
+    numArray[j+1] = last;
+}
+
+
+
+/***********************
+* heapSort y funciones asociadas
+***********************/
+// intercambia los valores
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = 2*i + 1; // left = 2*i + 1
+    int r = 2*i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// Funcion principal heap sort
+void heapSort(int numArray[], int size)
+{
+  // Crea pila
+  for (int i = size / 2 - 1; i >= 0; i--)
+      heapify(numArray, size, i);
+
+  // One by one extract an element from heap
+  for (int i=size-1; i>0; i--)
+  {
+      // Move current root to end
+      swap(&numArray[0], &numArray[i]);
+
+      // call max heapify on the reduced heap
+      heapify(numArray, i, 0);
+  }
 }
